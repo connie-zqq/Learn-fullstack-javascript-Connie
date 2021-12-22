326faf46b43c01344ff3ee1d41504138c8f9e8e9
Connie Zhang <zqq.bnu@gmail.com>
2021-12-22 09:36:52 -0800

    Update 2.3 - Creating an Express Server

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-import https from 'https';
+import config from './config.js'
+import apiRouter from './api/index.js'
+// import fs from 'fs';
 
-    https.get('https://www.sina.com', res => {
-    console.log('Response status code: ', res.statusCode);
+import express from 'express'
+const server = express();
 
-    res.on('data', chunk => {
-        console.log(chunk.toString());
-    });
+server.get('/', (req, res) => {
+    res.send('Hello Express');
 });
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+// server.get('/', (req, res) => {
+//   fs.readFile('./about.html', (err, data) => {
+//     res.send('Hello Express');
+//   });
+// });
+
+// server.get('/about.html', (req, res) => {
+//     res.send('The about page');
+//   });
+
+server.listen(config.port, () => {
+  console.info('Express listening on port ', config.port);
+});
+
+
