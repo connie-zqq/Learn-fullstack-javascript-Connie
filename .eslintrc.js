be1617d401aa7d225b3fc9e14fe78b15d1f74476
Connie Zhang <zqq.bnu@gmail.com>
2021-12-22 11:13:55 -0800

    Update 2.4 - Using the EJS template language

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
     // you can turn off that recommended rule with: 'react/prop-types': ['off']
     indent: ['error', 2, {SwitchCase: 1}], 
     'linebreak-style': ['error', 'unix'], 
-    quotes: ['error', 'single']
+    quotes: ['error', 'single'],
     semi: ['error', 'always'], 
     'no-console': ['warn', {allow: ['clear', 'info', 'error', 'dir', 'trace']}],
   },
--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
